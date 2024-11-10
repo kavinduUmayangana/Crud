0fdd012d1e205f1f3d4bf5d9bd17d843259cb671
Kavindu Umayangana <161000554+kavinduUmayangana@users.noreply.github.com>
2024-11-10 14:06:04 +0530

    use the postmapping for sending data in database

---
+++ src/main/java/com/example/fullstack_backend/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.example.fullstack_backend.controller;
+
+import com.example.fullstack_backend.model.User;
+import com.example.fullstack_backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+  @Autowired
+  private UserRepository userRepository;
+
+  @PostMapping("/user")
+  User newUser(@RequestBody User newUser){
+      return userRepository.save(newUser);
+  }
+
+
+}

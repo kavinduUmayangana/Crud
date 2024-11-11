6ff698d407087a6e4b3744b91924ae7c7c5111ce
Kavindu Umayangana <161000554+kavinduUmayangana@users.noreply.github.com>
2024-11-11 08:36:34 +0530

    exception handle for user not found

--- src/main/java/com/example/fullstack_backend/controller/UserController.java
+++ src/main/java/com/example/fullstack_backend/controller/UserController.java
@@ -3,14 +3,13 @@ package com.example.fullstack_backend.controller;
 import com.example.fullstack_backend.model.User;
 import com.example.fullstack_backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.nio.file.attribute.UserPrincipalNotFoundException;
 import java.util.List;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class UserController {
   @Autowired
   private UserRepository userRepository;
@@ -23,5 +22,11 @@ public class UserController {
   @GetMapping("/users")
   List<User> getAllUsers(){
     return userRepository.findAll();
+  }
+
+  @GetMapping("/user/{id}")
+  User getUserById(@PathVariable Long id){
+    return userRepository.findById(id)
+            .orElseThrow(()->  new UserNotFoundException(id));
   }
 }
